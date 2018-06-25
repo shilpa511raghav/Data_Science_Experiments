173cbac1f4ce5015e0f8dd103e94e87727a45268
shilpa511raghav <shilpa511raghav@gmail.com>
2018-06-25 06:27:20 +0530

    dictonary

---
+++ 12)_Dictonary_Python/code/dicton.py
@@ -0,0 +1,14 @@
+'''
+my_dictn ={1:'joseph', 'age':10 ,3:[2,4,6]}
+print(my_dictn)
+print(my_dictn.pop('age'))
+print(my_dictn.popitem())
+del my_dictn
+my_dictn= {}
+my_dictn[1]="neel"
+my_dictn['name'] ='riya'
+print(my_dictn.items())
+my_dictn['name']= 'rita'
+print(my_dictn)
+'''
+#program
